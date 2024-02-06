3ebae28fabbcd011bf83bc0df224f320706ab78f
Geovanny <s11575131@students.duvalschools.org>
2024-02-06 14:31:59 -0800

    TestProgram

---
+++ TestProject/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 25;
+        float myFloat = -1.234f;
+        bool yon = true;
+        string words = "so true";
+        Console.WriteLine("This is my Int: " + myInt);
+        Console.WriteLine("This is my Float: " + myFloat);
+        Console.WriteLine("This is my yes or no: " + yon);
+        Console.WriteLine("This is my words: " + words);
+
+        for (int i = 20; i > 0; i--) {
+            Console.WriteLine("You have " + i + " seconds left");
+        }
+        Console.WriteLine("Happy Birthday!");
+    }
+}
